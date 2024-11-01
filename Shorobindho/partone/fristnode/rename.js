becbe90b94d8670faa4fcab2e49ae00d3d7e2df5
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-01 11:01:06 +0600

    understand url and path module

--- Shorobindho/partone/fristnode/rename.js
+++ Shorobindho/partone/fristnode/rename.js
@@ -1,5 +1,15 @@
-import { renameSync, unlinkSync} from "fs"
+import { renameSync, unlinkSync, } from "fs"
+import { parse } from "url"
+import { basename, dirname,  } from "path";
 
 // renameSync("./db.json", "data.json");
 
-unlinkSync("index.html")
+// unlinkSync("index.html")
+
+const url = parse('https://sujon.com?name=kasem');
+const url2 = basename('https://sujon.com/box/fox/');
+const url3 = dirname('https://sujon.com/box/fox/');
+
+console.log(url)
+console.log(url2)
+console.log(url3)
