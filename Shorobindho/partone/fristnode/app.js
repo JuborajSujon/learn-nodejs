006c31d545e0080e07e0276aebe0827fcafd9be5
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-01 10:13:09 +0600

    file read  with fs module

--- Shorobindho/partone/fristnode/app.js
+++ Shorobindho/partone/fristnode/app.js
@@ -1,11 +1,19 @@
-const fs = require('fs')
+import { writeFile, writeFileSync, appendFile, appendFileSync,readFileSync } from "fs";
 
 
 // async funcion for write file 
-fs.writeFile("data.txt", "Hi Node, You are awesome", (err, data) => {
+writeFile("data.txt", "Hi Node, You are awesome", (err, data) => {
   console.log(err , data)
 })
 
-
 // sync funcion for write file 
-fs.writeFileSync('data2.txt', "Hello data file 2")
+writeFileSync('data2.txt', "Hello data file 2.")
+// sync update function 
+appendFileSync("data2.txt", "\nJs is the best")
+
+// async update function
+appendFile("data2.txt", "\nWe are devs", (err, data) => { })
+
+// sync function 
+const data = readFileSync('./db.json')
+console.log(data.toString())
