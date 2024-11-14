7e330cc1bf73622d4403ef6c0e14fa8edb9ea800
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-14 20:54:13 +0600

    understand variable alias

--- Programming Hero/module7/index.js
+++ Programming Hero/module7/index.js
@@ -1,4 +1,10 @@
-const add = require("./local-1.js");
+const {add, a} = require("./local-1.js");
+const { add: add2, a : a2 } = require("./local-2.js");
 
-const result = add(3,4);
+
+const result = add(3, 4);
+const result2 = add2(3, 4, 6);
 console.log(result);
+console.log("result2", result2);
+console.log(a)
+console.log("a2", a2)
