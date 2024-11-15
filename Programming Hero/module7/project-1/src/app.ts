8a2b7ab13abbd1bed7c15017672b48656c31d92c
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-15 12:12:00 +0600

    understand router

--- Programming Hero/module7/project-1/src/app.ts
+++ Programming Hero/module7/project-1/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express"
+import express, { NextFunction, Request, Response } from "express"
 const app = express()
 
 
@@ -6,9 +6,40 @@ const app = express()
 app.use(express.json())
 app.use(express.text())
 
+//
+const userRouter = express.Router();
+const courseRouter = express.Router();
 
-app.get('/', (req: Request, res: Response) => {
-  console.log(req.query)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/course", courseRouter);
+
+userRouter.get('/create-user', (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+  res.json({
+    success: true,
+    message: "User is created Sucessfully",
+    data: user,
+  })
+});
+
+
+courseRouter.post('/create-course', (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
+  res.json({
+    success: true,
+    message: "User is created Sucessfully",
+    data: course,
+  })
+})
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.hostname);
+  next();
+}
+
+app.get('/',logger, (req: Request, res: Response) => {
   res.send('Hello Developer nw!')
 })
 
