a0b5e8670578033c2420c1393bf5165d43860f79
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-15 11:45:38 +0600

    understand query and params

--- Programming Hero/module7/project-1/src/app.ts
+++ Programming Hero/module7/project-1/src/app.ts
@@ -1,9 +1,22 @@
-import express from "express"
+import express, { Request, Response } from "express"
 const app = express()
 
 
-app.get('/', (req, res) => {
+// parsers 
+app.use(express.json())
+app.use(express.text())
+
+
+app.get('/', (req: Request, res: Response) => {
+  console.log(req.query)
   res.send('Hello Developer nw!')
+})
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.json({
+    message: "Sucessfully received data"
+  })
 })
 
 export default app;
