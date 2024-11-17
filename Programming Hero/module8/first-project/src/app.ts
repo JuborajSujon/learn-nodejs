af50778fd4eb665de0fac8626d73c54d65a911a8
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-17 12:38:19 +0600

    student data send to mongodb

--- Programming Hero/module8/first-project/src/app.ts
+++ Programming Hero/module8/first-project/src/app.ts
@@ -1,12 +1,16 @@
 // const express = require('express')
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 
 const app: Application = express();
 
 //parser
 app.use(express.json());
 app.use(cors());
+
+//Appication routes
+app.use('/api/v1/students', StudentRoutes);
 
 const getHomeController = (req: Request, res: Response) => {
   res.send('Home Router');
--- Programming Hero/module8/first-project/src/app/modules/student/student.route.ts
+++ Programming Hero/module8/first-project/src/app/modules/student/student.route.ts
@@ -4,4 +4,6 @@ import { StudentControllers } from './student.controller';
 const router = express.Router();
 
 // will call controller func
-router.post('./create-student', StudentControllers.createStudent);
+router.post('/create-student', StudentControllers.createStudent);
+
+export const StudentRoutes = router;
