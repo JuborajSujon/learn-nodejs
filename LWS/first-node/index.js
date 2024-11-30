e21545dec1aeb7530d8aaad5b52ecdbeb07a463a
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-30 18:38:44 +0600

    passing arguments and this to listeners

--- LWS/first-node/index.js
+++ LWS/first-node/index.js
@@ -1,30 +1,26 @@
 const eventEmitter = require("events");
 
 class MyEmitter extends eventEmitter {
-  value = "Hello";
-
-  set(value) {
-    this.value = value;
+  value = 0;
+  set() {
+    this.value = 1;
   }
-
   get() {
     return this.value;
-  }
-
-  constructor() {
-    super();
   }
 }
 
 const emitter = new MyEmitter();
 
-// Register a listener
+// register event handler
 emitter.on("start", function (a, b) {
-  console.log(a, b, this, this === emitter);
+  console.log("start event fired");
+  console.log(a + b);
   console.log(this.get());
-  this.set("World");
+  console.log(this.set());
   console.log(this.get());
+  console.log(this.value + this.get());
 });
 
-// Emit an event
+// fire event
 emitter.emit("start", 1, 2);
