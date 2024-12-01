3ff4ae0385ca0838947ad5a14555d9dcbcdefda9
Juboraj Sujon <mdsujon72@gmail.com>
2024-12-01 17:35:36 +0600

    handling events only onec

--- LWS/first-node/index.js
+++ LWS/first-node/index.js
@@ -1,24 +1,18 @@
 const eventEmitter = require("events");
 
-class MyEmitter extends eventEmitter {
-  value = 0;
-  set() {
-    this.value = 1;
-  }
-  get() {
-    return this.value;
-  }
-}
+class MyEmitter extends eventEmitter {}
 
 const emitter = new MyEmitter();
 
+let m = 0;
+
 // register event handler
-emitter.on("start", (a, b) => {
-  console.log("start event", this);
-  setTimeout(() => {
-    console.log("After 3 seconds", a, b);
-  }, 3000);
+emitter.once("start", () => {
+  console.log(++m);
 });
 
-// fire event
-emitter.emit("start", 1, 2);
+// first event handler
+emitter.emit("start");
+
+// second event handler
+emitter.emit("start");
