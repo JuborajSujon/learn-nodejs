221c4bbc76311feb903fb48129e3d97a8b755b09
Juboraj Sujon <mdsujon72@gmail.com>
2024-12-05 15:28:45 +0600

    node path module

--- LWS/first-node/path.js
+++ LWS/first-node/path.js
@@ -1,7 +1,24 @@
 const path = require("path");
 
-const url = "E:\\Node JS\\Github File\\LWS\\first-node\\path.js";
+const url = "E:\\Node JS\\Github File\\LWS\\first-node\\index.js";
 
-const result = path.basename(url);
+// path.basename
+const result = path.basename(url); // index.js
 
-console.log(result);
+// path.basename(path[,suffix])
+const result1 = path.basename(url, ".js"); // index
+
+// path.delimiter
+const result3 = process.env.PATH.split(path.delimiter);
+
+// path.dirname(path)
+const result4 = path.dirname(
+  "E:\\Node JS\\Github FileLWS\\first-node\\path.js"
+);
+
+// const result5 = path.matchesGlob(url, "/index.js");
+
+const result6 = path.isAbsolute(url);
+const result7 = path.isAbsolute("/index.js");
+
+console.log(result7);
